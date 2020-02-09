bbe7574903e5184fca727257444f895b173b4f24
Alec Pervushin <alec.ext@gmail.com>
2020-02-09 10:31:23 +0300

    remove ps scripts

--- scripts/index.psm1
+++
@@ -1,5 +0,0 @@
-﻿$moduleFunctions = @(Get-ChildItem -Path "$PSScriptRoot/*" -Exclude index.* -Include *.psm1 -ErrorAction SilentlyContinue)
-
-foreach ($module in $moduleFunctions) {
-  Import-Module $module.FullName
-}
